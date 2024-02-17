90a7677c0f7af727fab2c6f42ae8572a8cc55971
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-02-17 14:03:16 -0800

    Added working PWM demo code

--- src/CPiCar.cpp
+++ src/CPiCar.cpp
@@ -7,15 +7,27 @@
 #include "../include/CPiCar.h"
 
 CPiCar::CPiCar() {
+    _output_pins.push_back(STEERING_PIN_BCM);
+    _output_pins.push_back(THROTTLE_PIN_BCM);
+    _control.init_gpio(_input_pins, _output_pins);
 }
 
 CPiCar::~CPiCar() {
+    _control.stop();
 }
 
 void CPiCar::draw() {
     std::cout << "draw" << std::endl;
+    for (int duty = 0; duty < 100000; duty++) {
+        gpioHardwarePWM(STEERING_PIN_BCM,100,100000 + duty);
+        gpioHardwarePWM(THROTTLE_PIN_BCM,100,200000 - duty);
+    }
+    for (int duty = 0; duty < 100000; duty++) {
+        gpioHardwarePWM(STEERING_PIN_BCM,100,200000 - duty);
+        gpioHardwarePWM(THROTTLE_PIN_BCM,100,100000 + duty);
+    }
 }
 
 void CPiCar::update() {
-    std::cout << "update" << std::endl;
+//    std::cout << "update" << std::endl;
 }
