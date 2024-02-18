fe05812b2987689ea708f24523faa33e6d97f0ac
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-02-18 01:07:32 -0800

    Added code to search for joystick and fliter out button/axis events

--- include/CPiControl.h
+++ include/CPiControl.h
@@ -6,13 +6,22 @@
 
 #pragma once
 
+#define HEARTBEAT_PIN_BCM 17
 #define STEERING_PIN_BCM 12
 #define THROTTLE_PIN_BCM 13
 
 #include <iostream>
 #include <vector>
-#include <libevdev/libevdev.h>
+#include <filesystem>
 #include <pigpio.h>
+#include <thread>
+
+// Linux specific includes
+#include <libevdev/libevdev.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 class CPiControl {
 public:
@@ -21,6 +30,26 @@ public:
     void stop();
     void init_gpio(const std::vector<int> &input_pins, std::vector<int> &output_pins);
     void init_evdev_joystick();
+    int js_has_new_event();
+    void js_get_next_thing();
+    std::vector<int> get_vals() { return _vals;};
 private:
+    std::vector<std::string> _event_codes = {
+            "ls_x",
+            "ls_y",
+            "lt",
+            "rs_x",
+            "rs_y",
+            "rt",
+            "dpad_x",
+            "dpad_y"
+    };
+    std::vector<int> _vals {
+            0,
+            0
+    };
     std::string _device;
+    struct libevdev *_evdev_dev = nullptr;
+    struct input_event _evdev_dev_event = {};
+    int _joystick_fd;
 };
--- src/CPiControl.cpp
+++ src/CPiControl.cpp
@@ -29,9 +29,64 @@ void CPiControl::init_gpio(const std::vector<int> &input_pins, std::vector<int> 
 }
 
 void CPiControl::init_evdev_joystick() {
+    std::cout << "joystick init" << std::endl;
 
+    while (_device.empty()) {
+        std::cout << "Searching for joystick..." << std::endl;
+        // partially from https://stackoverflow.com/a/2340309
+        for (const auto & entry : std::filesystem::directory_iterator("/dev/input")) {
+            if (entry.path().string().find("event") != std::string::npos) {
+                // from libevdev sample code
+                auto fd = open(entry.path().string().c_str(), O_RDONLY|O_NONBLOCK);
+                struct libevdev *dev = nullptr;
+                int rc = libevdev_new_from_fd(fd, &dev);
+
+                // looking for the dualshock 4
+                if (std::string(libevdev_get_name(dev)) == "Wireless Controller") {
+                    _device = entry.path().string();
+                }
+                int close(fd);
+            }
+        }
+        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(100));
+    }
+    std::cout << "Joystick found at " << _device << std::endl;
+    _joystick_fd = open(_device.c_str(), O_RDONLY|O_NONBLOCK);
+
+    // TODO: handle if joystick cannot connect here
+    libevdev_new_from_fd(_joystick_fd, &_evdev_dev);
 }
 
 void CPiControl::stop() {
     gpioTerminate();
+}
+
+int CPiControl::js_has_new_event() {
+    if (_evdev_dev != nullptr) return libevdev_has_event_pending(_evdev_dev);
+    return 0;
+}
+
+void CPiControl::js_get_next_thing() {
+    if (_evdev_dev != nullptr && libevdev_has_event_pending(_evdev_dev)) {
+        libevdev_next_event(_evdev_dev, LIBEVDEV_READ_FLAG_NORMAL, &_evdev_dev_event);
+        if (_evdev_dev_event.type == EV_ABS)
+            switch (_evdev_dev_event.code) {
+                case 0:
+                    std::cout << "Got input_event";
+                    std::cout << " type=" << _evdev_dev_event.type;
+                    std::cout << " code=" << "left analog x";
+                    std::cout << " value=" << _evdev_dev_event.value << std::endl;
+                    _vals[0] = _evdev_dev_event.value;
+                    break;
+                case 4:
+                    std::cout << "Got input_event";
+                    std::cout << " type=" << _evdev_dev_event.type;
+                    std::cout << " code=" << "right analog y";
+                    std::cout << " value=" << _evdev_dev_event.value << std::endl;
+                    _vals[1] = _evdev_dev_event.value;
+                    break;
+                default:
+                    break;
+        }
+    }
 }
