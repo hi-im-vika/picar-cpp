0e135124bb0aedb6840e8ff827fedfa650100e08
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-02-18 19:58:02 -0800

    Added a class to make logging easier

---
+++ include/CLogger.h
@@ -0,0 +1,32 @@
+/**
+ * CLogger.h - new file
+ * 2024-02-18
+ * vika <https://github.com/hi-im-vika>
+ */
+
+#pragma once
+#include <iostream>
+#include <chrono>
+
+/**
+ * @brief   This is a class to make outputting logs easier.
+ * @details This is intended to be a singleton beacuse only one object is required to show logs, will actually do later
+ * @author  vika
+ */
+class CLogger {
+public:
+    CLogger() = default;
+    ~CLogger() = default;
+    void show_log(std::string source, std::string level, std::string content);
+private:
+//protected:
+//    CLogger() = default;
+//    CLogger *_clogger;
+//public:
+//    CLogger(CLogger &other) = delete;
+//    void operator = (const CLogger &) = delete;
+//    static CLogger *get_instance() {
+//        if (_clogger
+//    }
+//    void show_log();
+};
---
+++ src/CLogger.cpp
@@ -0,0 +1,27 @@
+/**
+ * CLogger.cpp - new file
+ * 2024-02-18
+ * vika <https://github.com/hi-im-vika>
+ */
+
+#include "../include/CLogger.h"
+
+
+//CLogger *CLogger::get_instance() {
+//    if (_clogger == nullptr) {
+//        _clogger = new CLogger();
+//    }
+//    return _clogger;
+//}
+
+void CLogger::show_log(std::string source, std::string level, std::string content) {
+    auto now = std::chrono::system_clock::now();
+    std::cout << std::chrono::system_clock::to_time_t(now);
+    std::cout << " [";
+    std::cout << level;
+    std::cout << "][";
+    std::cout << source;
+    std::cout << "] ";
+    std::cout << content;
+    std::cout << std::endl;
+}
