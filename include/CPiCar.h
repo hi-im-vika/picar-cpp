f5a15c619511e17c8b342905f3d8bf14bd3c8058
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-02-18 20:27:12 -0800

    Multithreaded the find joystick feature

--- include/CPiCar.h
+++ include/CPiCar.h
@@ -15,12 +15,17 @@ public:
     void draw() override;
     void update() override;
     void heartbeat();
+    void joystick();
+    static void joystick_thread(CPiCar *who_called_me);
 private:
     std::vector<int> _last_vals = _control.get_vals();
     int _trim = 0;
     bool _dpax_prev = false;
     bool _dpay_prev = false;
     bool _do_heartbeat = true;
-    std::chrono::steady_clock::time_point _start;       ///< Start time for fake interrupts.
-    long long _last_time;
+    bool _do_draw = false;
+    bool _found_joystick = false;
+    long long _last_time = 0;
+    std::chrono::steady_clock::time_point _start;       ///< Start time to schedule interrupts
+    long long _last_heartbeat = 0;
 };
--- src/CPiCar.cpp
+++ src/CPiCar.cpp
@@ -11,8 +11,8 @@ CPiCar::CPiCar() {
     _output_pins.push_back(THROTTLE_PIN_BCM);
     _output_pins.push_back(HEARTBEAT_PIN_BCM);
     _control.init_gpio(_input_pins, _output_pins);
-    gpioHardwarePWM(STEERING_PIN_BCM,100,150000);
-    gpioHardwarePWM(THROTTLE_PIN_BCM,100,150000);
+    gpioHardwarePWM(STEERING_PIN_BCM, 100, 150000);
+    gpioHardwarePWM(THROTTLE_PIN_BCM, 100, 150000);
 //    gpioSetPWMrange(STEERING_PIN_BCM, 40000);
 //    gpioSetPWMrange(THROTTLE_PIN_BCM,40000);
 //    gpioSetPWMfrequency(STEERING_PIN_BCM,100);
@@ -20,17 +20,30 @@ CPiCar::CPiCar() {
 //    gpioPWM(STEERING_PIN_BCM,6000);
 //    gpioPWM(THROTTLE_PIN_BCM,6000);
 //    gpioWrite(HEARTBEAT_PIN_BCM, PI_ON);
-    _control.init_evdev_joystick();
+    std::thread thread_for_joysticking(joystick_thread, this);
+    thread_for_joysticking.detach();
 }
 
 CPiCar::~CPiCar() {
     _control.stop();
 }
 
+// yeah its draw but you know what i mean
 void CPiCar::draw() {
     // TODO: implement safe mode on controller disconnect
-    if(!_control.get_device_path().empty()) {
-        _last_vals = _control.get_vals();
+    if (!_do_draw) {
+        // send a log every second, no need for all the time
+        long long now = (int) std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now() - CPiCar::_start).count();
+        if ((!(now % 1000)) && _last_time != now) {
+            _logger.show_log("CPiCar", "WARN", "_draw not asserted, defaulting to neutral values");
+            _last_time = now;
+        } else {
+            _last_time = now;
+        }
+        gpioHardwarePWM(STEERING_PIN_BCM, 100, 150000);
+        gpioHardwarePWM(THROTTLE_PIN_BCM, 100, 150000);
+    } else {
 
         // trim adjustment
         if (_last_vals[2] != 0 && !_dpax_prev) {
@@ -89,41 +102,53 @@ void CPiCar::draw() {
 }
 
 void CPiCar::update() {
-//    std::cout << "update" << std::endl;
     if (_do_heartbeat) heartbeat();
+    if (_found_joystick) _control.js_get_next_thing();
+}
 
-    bool found = false;
+void CPiCar::heartbeat() {
+    long long now = (int) std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now() - CPiCar::_start).count();
+    if ((!(now % 1000) && now != _last_heartbeat) || (!((now % 1000) - 200) && now != _last_heartbeat)) {
+        gpioWrite(HEARTBEAT_PIN_BCM, PI_ON);
+        _last_heartbeat = now;
+    }
+    if ((!((now % 1000) - 50) && now != _last_heartbeat) || (!((now % 1000) - 250) && now != _last_heartbeat)) {
+        gpioWrite(HEARTBEAT_PIN_BCM, PI_OFF);
+        _last_heartbeat = now;
+    }
+}
+
+void CPiCar::joystick() {
+    /**
+     * 1. check to see if saved joystick is still there. if yes, go to next iteration
+     * 2. otherwise, no joystick is there. go into joystick finding loop.
+     */
+    _found_joystick = false;
     for (const auto &entry: std::filesystem::directory_iterator("/dev/input")) {
         if (entry.path().string() == _control.get_device_path()) {
-            found = true;
+            _found_joystick = true;
         }
     }
-
-    if (found) {
-        _control.js_get_next_thing();
-    } else {
-        // TODO: implement safe mode on controller disconnect
-        gpioHardwarePWM(STEERING_PIN_BCM,100,150000);
-        gpioHardwarePWM(THROTTLE_PIN_BCM,100,150000);
-        _control.stop();
+    if (!_found_joystick) {
+        // set flag to stop updating
+        _do_draw = false;
         _do_heartbeat = false;
+        _control.stop();
+        _logger.show_log("CPiCar", "WARN", "Joystick not found");
         gpioWrite(HEARTBEAT_PIN_BCM, PI_ON);
         while (_control.get_device_path().empty()) {
             _control.init_evdev_joystick();
         }
-        _do_heartbeat = true;
+        _logger.show_log("CPiCar", "INFO", "Joystick found at " + _control.get_device_path());
         gpioWrite(HEARTBEAT_PIN_BCM, PI_OFF);
+        _do_heartbeat = true;
+        _do_draw = true;
     }
 }
 
-void CPiCar::heartbeat() {
-    long long now = (int) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - CPiCar::_start).count();
-    if ((!(now % 1000) && now != _last_time) || (!((now % 1000) - 200) && now != _last_time)) {
-        gpioWrite(HEARTBEAT_PIN_BCM, PI_ON);
-        _last_time = now;
-    }
-    if ((!((now % 1000) - 50) && now != _last_time) || (!((now % 1000) - 250) && now != _last_time)) {
-        gpioWrite(HEARTBEAT_PIN_BCM, PI_OFF);
-        _last_time = now;
+void CPiCar::joystick_thread(CPiCar *who_called_me) {
+    while (!who_called_me->_do_exit) {
+        who_called_me->joystick();
     }
 }
